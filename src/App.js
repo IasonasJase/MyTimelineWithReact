deb2106429b930dbdc1adf3d2da12c9dc9e8ec84
IasonasJase <88947991+IasonasJase@users.noreply.github.com>
2022-07-04 14:42:45 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./timelineCss.css";
+import React from "react";
+import { useState, useEffect } from "react";
+import TimelineList from "./TimelineList";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [timeline, setTimeline] = useState(JSON.parse(localStorage.getItem("timeline-items")) || []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let index = timeline.length || 0;
+    if (input.trim() !== "") {
+      const newEvent = {
+        id: ++index,
+        text: input,
+        date: new Date().toLocaleString(),
+      };
+      const events = timeline;
+      events.push(newEvent);
+      setTimeline(events);
+    }
+    setInput("");
+    console.log(timeline);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("timeline-items", JSON.stringify(timeline));
+  }, [timeline]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="wrapper">
+      <form className="event-form" onSubmit={handleSubmit}>
+        <div className="input-field">
+          <input
+            type="text"
+            id="input--add"
+            placeholder="e.g. Fifa World Cup"
+            onChange={(e) => setInput(e.target.value)}
+            value={input}
+          />
+          <button type="submit" className="button">
+            Add
+          </button>
+          <button className="button" onClick={() => setTimeline([])}>
+            Clear
+          </button>
+        </div>
+      </form>
+      {timeline.map((item, i) => (
+        <TimelineList key={item.id} timeline={item} index={i} />
+      ))}
+    </main>
   );
 }
 
---
+++ src/TimelineList.js
@@ -0,0 +1,27 @@
+import React from "react";
+import football from "./photos/football.svg";
+import headphones from "./photos/headphones.svg";
+import ipod from "./photos/ipod.svg";
+import music from "./photos/music.svg";
+
+const TimelineList = ({ timeline, index }) => {
+  const iconsArray = [football, headphones, ipod, music];
+  const conditionalClassName = index % 2 === 0 ? "left" : "right";
+  return (
+    <div className="timeline">
+      <div className={`container ${conditionalClassName}`}>
+        <div className="date">{timeline.date}</div>
+
+        <div className="orange-box">
+          <p>{timeline.text}</p>
+        </div>
+        <div className="img-border">
+          <img alt="football-icon" src={iconsArray[index % 4]} className="fa-icon" />
+        </div>
+        <div className="black-box"></div>
+      </div>
+    </div>
+  );
+};
+
+export default TimelineList;
